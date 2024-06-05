f6301a6ccb101ae948ef63b4c65e13b74213384b
katungi <daniel.katungi@fin.africa>
2024-06-05 10:32:42 +0300

    feat: capture correct tsconfigs

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import JestHasteMap from 'jest-haste-map';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import fs from 'fs'
+
+const rootMap = join(dirname(fileURLToPath(import.meta.url)), 'product');
+
+const hasteMap = new JestHasteMap.default({
+    extensions: ['js', 'jsx', 'ts', 'tsx'],
+    name: 'fray',
+    rootDir: rootMap,
+    roots: [rootMap],
+})
