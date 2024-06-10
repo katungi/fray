3e6ee5ceeb0ab2475662687fc198655a39c8b7c5
katungi <daniel.katungi@fin.africa>
2024-06-10 16:37:25 +0300

    release maybe?

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Command, Flags, Args } from "@oclif/core";
 import chalk from "chalk";
 import { bundle } from "./bundler/bundler";
 
-class Fray extends Command {
+export default class Fray extends Command {
     static description = "Fray - A Javascript module bundler";
 
     static flags = {
